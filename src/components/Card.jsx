7b60e67abaa7750576b165fbf77c5ec4aa3bafdf
prvharshe <pranav.harshe94@gmail.com>
2022-07-05 22:17:16 +0530

    completed card section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,18 +2,31 @@ import React from 'react'
 
 const Card = (props) => {
     return (
-        <div className="w-96 mt-6 p-5 py-20">
-            <div className="h-30 bg-contain border border-solid border-black">
-                <img className="bg-contain" src={props.image} alt=""/>
+        <div className="card mt-6 bg-white shadow-lg">
+            <div className="shadow-sm">
+                <img className="card-img" src={props.image} alt=""/>
             </div>
-            <div>
-                <span>{props.price}</span>
-                <span>{props.address}</span>
-            </div>
-            <div>
-                <span>{props.beds}</span>
-                <span>{props.baths}</span>
-                <span>{props.area}</span>
+            <div className="p-5 space-y-3">
+                <div className="flex justify-between">
+                    <div className="flex items-center">
+                        <span className="text-2xl text-indigo-700 font-bold pr-1">{props.price}</span>
+                        <span className="text-gray-400 font-medium">/month</span>
+                    </div>
+                    <div className="flex items-center bg-indigo-600 rounded-2xl px-2">
+                        <span className="text-white font-medium">{props.type}</span>
+                    </div>
+                </div>
+                <div>
+                    <span className="text-3xl text-indigo-900 font-bold">{props.name}</span>
+                </div>
+                <div>
+                    <span className="text-gray-400 font-medium">{props.address}, {props.location}</span>
+                </div> <hr></hr>
+                <div className="flex justify-between text-gray-400 font-medium">
+                    <span>{props.beds} Beds</span>
+                    <span>{props.baths} Bathrooms</span>
+                    <span>{props.area} sqft</span>
+                </div>
             </div>
         </div>
     )
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,19 +7,24 @@ function Content() {
     const[properties, setProperties] = React.useState(data);
 
     return (
-        <div className="sm:mx-10 sm:my-5 mx-5 my-2 ">
+        <div className="sm:mx-10 sm:my-5 mx-5 my-2 pb-7">
             <span className="text-4xl font-bold">Search properties to rent</span>
 
-            <div className="grid grid-cols-4 flex-wrap justify-center">
+            
+
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-1 flex-wrap justify-center">
                 {properties.map(key => (
                     <Card 
                         key={key} 
+                        name={key.name}
                         image={key.image} 
                         price={key.price} 
+                        location={key.location}
                         address={key.address} 
+                        type={key.type}
                         beds={key.beds} 
                         baths={key.baths} 
-                        area={key.area}
+                        area={key.sqft}
                     />
                 ))}
             </div>
